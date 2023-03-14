629b8e923ea34579755db66b9520875d2f2abeb3
KesterDaniel <kesterdan17@gmail.com>
2023-03-14 12:56:12 +0100

    recording and saving voice to variable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import speech_recognition as sr
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -11,9 +12,38 @@ from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 load_dotenv()
 
-chrome_driver_path = "C:\development\chromedriver.exe"
-chr_options = webdriver.ChromeOptions()
-chr_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(chrome_driver_path), options=chr_options)
-driver.get("https://www.youtube.com/")
+# chrome_driver_path = "C:\development\chromedriver.exe"
+# chr_options = webdriver.ChromeOptions()
+# chr_options.add_experimental_option("detach", True)
+#
+# driver = webdriver.Chrome(service=Service(chrome_driver_path), options=chr_options)
+# # driver.get("https://www.youtube.com/")
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+
+def listen():
+    with mic as source:
+        audio = r.listen(source)
+        try:
+            data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("try again")
+            listen()
+        else:
+            return data
+
+
+phrase = listen()
+
+
+
+# harvard = sr.AudioFile("harvard.wav")
+# with harvard as source:
+#     r.adjust_for_ambient_noise(source)
+#     audio = r.record(source)
+#
+# data = r.recognize_google(audio)
+# print(data)
